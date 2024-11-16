bb051b11404ffdbb713acefb3a7eaa36534d4640
mai <fujisawamai21@gmail.com>
2024-11-16 14:18:35 +0900

    enhance visual and accessibility

--- 3_age-calculator/src/App.vue
+++ 3_age-calculator/src/App.vue
@@ -2,19 +2,22 @@
 import { ref, computed } from 'vue';
 const birthdate = ref<string>('');
 const calcAge = computed(() => {
-  const diffSec = Math.floor((new Date().getTime() - new Date(birthdate.value).getTime()) / 1000); //秒
+  const diffSec = Math.floor((new Date().getTime() - new Date(birthdate.value).getTime()) / 1000);
   const age = Math.floor(diffSec / 3600 / 24 / 365);
   return age;
 })
 </script>
 
 <template>
-  <main>
-    <div>
-      <h1>Age Calculator</h1>
-      <label for="birthdate">Enter your birthdate</label>
-      <input type="date" id="birthdate" v-model="birthdate" />
+  <main class="h-screen w-screen flex flex-col justify-center items-center">
+    <div class="w-[600px] bg-white rounded-md shadow-md p-5 text-center flex flex-col gap-8">
+      <h1 class="font-semibold text-4xl">Age Calculator</h1>
+      <div class="flex flex-col gap-4 items-center">
+        <label for="birthdate" class="font-semibold text-xl">Enter your birthdate</label>
+        <input type="date" id="birthdate" :max="new Date().toISOString().split('T')[0]" v-model="birthdate" required
+          class="w-1/2 border border-gray-300 rounded p-2" />
+      </div>
+      <p class="font-semibold text-2xl">Your age is {{ isNaN(calcAge) ? "?" : calcAge }} years old</p>
     </div>
-    <div>Your age is {{ calcAge }} years old</div>
   </main>
 </template>
