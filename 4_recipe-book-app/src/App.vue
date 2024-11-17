6c6bd02b48e191fbb25fa76e847827ccd797f626
mai <fujisawamai21@gmail.com>
2024-11-17 15:26:35 +0900

    一部指摘を反映

--- 4_recipe-book-app/src/App.vue
+++ 4_recipe-book-app/src/App.vue
@@ -7,14 +7,22 @@ import { onMounted, ref } from 'vue';
 
 const recipes = ref<Recipe[]>([]);
 const applicationId = import.meta.env.VITE_APPLICATION_ID;
+if (!applicationId) {
+    throw new Error("VITE_APPLICATION_ID is not defined in environment variables");
+}
+const API_URL = "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426";
+const isLoading = ref(false);
 
 const fetchRecipes = async function () {
+    isLoading.value = true;
     try {
-        const res = await axios.get(`https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?format=json&applicationId=${applicationId}`);
+        const res = await axios.get(`${API_URL}?format=json&applicationId=${applicationId}`);
         recipes.value = res.data.result;
         console.log(recipes.value);
     } catch (err) {
         console.error(err);
+    } finally {
+        isLoading.value = false;
     }
 }
 onMounted(() => {
@@ -27,7 +35,8 @@ onMounted(() => {
         <h1>Recipe Book App</h1>
     </header>
     <main>
-        <ul class="flex flex-col gap-4 items-center">
+        <div v-if="isLoading">Loading...</div>
+        <ul v-else class="flex flex-col gap-4 items-center">
             <li v-for="recipe in recipes" :key="recipe.recipeId">
                 <RecipeCard :recipe="recipe" />
             </li>
--- 4_recipe-book-app/src/components/RecipeCard.vue
+++ 4_recipe-book-app/src/components/RecipeCard.vue
@@ -6,12 +6,13 @@ defineProps<{
 </script>
 <template>
     <div class="flex items-center gap-4 w-full bg-white shadow-md">
-        <img :src="recipe.foodImageUrl" alt="Food image" class=" w-[150px] h-[150px] object-cover" loading="lazy">
+        <img :src="recipe.foodImageUrl" :alt="`Image of ${recipe.recipeTitle}`"
+            class=" w-[150px] h-[150px] object-cover" loading="lazy">
         <h2 class="max-w-60 font-bold text-lg">{{ recipe.recipeTitle }}</h2>
         <p class="w-[700px] text-gray-600">
             {{ recipe.recipeDescription }}
         </p>
-        <a class="bg-orange-200 py-3 px-5 w-40" :href="recipe.recipeUrl">
+        <a class="bg-orange-200 py-3 px-5 w-40" :href="recipe.recipeUrl" targe="_blank">
             View detail
         </a>
     </div>
